418060cd8e3dbe30be86e8a1028ca2c60975323a
Shude Li <islishude@gmail.com>
2020-08-30 09:31:48 +0800

    Update multisig.sol

--- multisig.sol
+++ multisig.sol
@@ -6,44 +6,40 @@ contract MultiSig {
     uint256 public nonce;
     uint8 public threshold;
 
-    address[] internal _members;
     mapping(address => bool) public membership;
     mapping(uint256 => mapping(address => bool)) confirmation;
-
-    event Withdraw(
-        address indexed from,
-        address indexed to,
-        uint256 indexed value
-    );
 
     modifier OnlyMember {
         require(membership[msg.sender], "no permission to execute");
         _;
     }
 
+    modifier OnlyContract(address addr) {
+        assembly {
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+        _;
+    }
+
     constructor(uint8 M, address[] memory members) payable {
         require(M > 0, "threshold must greater than zero");
-        threshold = M;
-        for (uint256 i = 0; i < members.length; ++i) {
-            if (membership[members[i]] || members[i] == address(0x0)) {
-                continue;
-            }
-            membership[members[i]] = true;
-            _members.push(members[i]);
-        }
         require(
-            threshold <= _members.length,
+            threshold <= members.length,
             "threshold must less than length of member"
         );
+        threshold = M;
+        for (uint256 i = 0; i < members.length; ++i) {
+            require(members[i] != address(0x0), "empty address");
+            require(!membership[members[i]], "duplicate address");
+            membership[members[i]] = true;
+        }
     }
 
     receive() external payable {}
 
-    function members() public view returns (address[] memory) {
-        return _members;
-    }
-
-    function checkSig(
+    function verify(
         bytes4 func,
         uint8[] memory v,
         bytes32[] memory r,
@@ -55,12 +51,20 @@ contract MultiSig {
     ) internal {
         require(
             v.length == r.length && r.length == s.length,
-            "Invalid signs length"
+            "Invalid signatures length"
         );
         require(v.length >= threshold, "Insufficient number of signatures");
         require(_to != address(this), "Can not transfer to current contract");
         for (uint256 i = 0; i < threshold; ++i) {
-            bytes memory data = abi.encode(func, msg.sender, _token, _from, _to, _value, nonce);
+            bytes memory data = abi.encode(
+                func,
+                msg.sender,
+                _token,
+                _from,
+                _to,
+                _value,
+                nonce
+            );
             bytes32 hash = keccak256(data);
             address member = ecrecover(hash, v[i], r[i], s[i]);
             require(membership[member], "no permission to sign");
@@ -75,17 +79,10 @@ contract MultiSig {
         bytes32[] memory s,
         address payable _to,
         uint256 _value
-    ) external OnlyMember returns (bool) {
-        checkSig(0x4b239a29, v, r, s, address(0x0), address(this), _to, _value);
-        _to.transfer(_value);
-        emit Withdraw(msg.sender, _to, _value);
-        return true;
-    }
-
-    function isContract(address addr) internal view {
-        assembly {
-            if iszero(extcodesize(addr)) { revert(0, 0) }
-        }
+    ) external OnlyMember {
+        verify(0x4b239a29, v, r, s, address(0x0), address(this), _to, _value);
+        (bool success, ) = _to.call{value: _value}(new bytes(0));
+        require(success, "ETHER_TRANSFER_FAILED");
     }
 
     function ERC20Transfer(
@@ -95,11 +92,15 @@ contract MultiSig {
         address _token,
         address _to,
         uint256 _value
-    ) external OnlyMember {
-        isContract(_token);  
-        checkSig(0xa9059cbb, v, r, s, _token, address(this), _to, _value);
-        (bool success, bytes memory data) = _token.call(abi.encodeWithSelector(0xa9059cbb, _to, _value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'ERC20_TRANSFER_FAILED');
+    ) external OnlyMember OnlyContract(_token) {
+        verify(0xa9059cbb, v, r, s, _token, address(this), _to, _value);
+        (bool success, bytes memory data) = _token.call(
+            abi.encodeWithSelector(0xa9059cbb, _to, _value)
+        );
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "ERC20_TRANSFER_FAILED"
+        );
     }
 
     function ERC20TransferFrom(
@@ -110,11 +111,15 @@ contract MultiSig {
         address _from,
         address _to,
         uint256 _value
-    ) external OnlyMember {
-        isContract(_token);  
-        checkSig(0x23b872dd, v, r, s, _token, _from, _to, _value);
-        (bool success,bytes memory data ) = _token.call(abi.encodeWithSelector(0x23b872dd, _from, _to, _value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'ERC20_TRANSFER_FROM_FAILED');
+    ) external OnlyMember OnlyContract(_token) {
+        verify(0x23b872dd, v, r, s, _token, _from, _to, _value);
+        (bool success, bytes memory data) = _token.call(
+            abi.encodeWithSelector(0x23b872dd, _from, _to, _value)
+        );
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "ERC20_TRANSFER_FROM_FAILED"
+        );
     }
 
     function ERC20Approve(
@@ -124,10 +129,14 @@ contract MultiSig {
         address _token,
         address _spender,
         uint256 _value
-    ) external OnlyMember {
-        isContract(_token);
-        checkSig(0x095ea7b3, v, r, s, _token, address(this), _spender, _value);
-        (bool success, bytes memory data) = _token.call(abi.encodeWithSelector(0x994ead30, _spender, _value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'ERC20_APPROVE_FAILED');
+    ) external OnlyMember OnlyContract(_token) {
+        verify(0x095ea7b3, v, r, s, _token, address(this), _spender, _value);
+        (bool success, bytes memory data) = _token.call(
+            abi.encodeWithSelector(0x994ead30, _spender, _value)
+        );
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "ERC20_APPROVE_FAILED"
+        );
     }
 }
